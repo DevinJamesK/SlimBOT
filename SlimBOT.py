647f4a0565bbb5246c43218bab3acfd3fcacb622
Devin Kloos <DevinJamesK@users.noreply.github.com>
2014-03-16 00:59:07 -0400

    Update SlimBOT.py
    Made it G rated and added a help command, also cleaned up the comments a bit more.

--- SlimBOT.py
+++ SlimBOT.py
@@ -30,11 +30,12 @@ import random
 ##################################################################################################
 
 #Change these to whatever suits your needs
-network = 'CHANGE THIS TO YOUR NETWORK'
-channel = "#CHANGE THIS TO YOUR CHANNEL"
-nick = 'slimBOT'
+network = 'CHANGE THIS TO YOUR NETWORK'  #<---------------------------------------------CHANGE ME!
+channel = "#CHANGE THIS TO YOUR CHANNEL" #<---------------------------------------------CHANGE ME!
+nick = 'slimBOT' #<--------------------------------------------------YOU CAN CHANGE ME IF YOU WANT
 
 #You should never have to change the port for a basic setup
+#port 6667 isa default port for IRC
 port = 6667
 
 #Setting up the socket below, this too you should not ever have to change
@@ -42,8 +43,6 @@ irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((network,port ))
 
 #This prints all incoming data from the IRC server
-#The number is the Buffer (how many characters socket takes in at a time
-print irc.recv(4096)
 
 ##################################################################################################
 ###-------------------------------CONNECTING AND COMMAND MUSTS---------------------------------###
@@ -53,10 +52,15 @@ irc.send('NICK ' + nick + '\r\n')
 irc.send('USER ' + nick + ' ' + nick + ' ' + nick + ' ' + ':A Simple Python IRC\r\n')
 irc.send('JOIN ' + channel + '\r\n')
 
+# Once the bot has joined the channel
 irc.send('PRIVMSG ' + channel + ' :Hello World, I\'m ' + nick + '\r\n')
 
 while True:
-    data = irc.recv(4096)
+    #The number is the Buffer (how many characters socket takes in at a time)
+    #HINT: The Buffer should be about as many characters are in the input string from the socket 
+    # + some room for longer usernames + the longest command 
+    #When in doubt, too long is better than too short
+    data = irc.recv(64)
     if data.find('PING') != -1:
         irc.send('PONG ' + data.split()[1] + '\r\n')
 
@@ -66,12 +70,16 @@ while True:
 
     #Tells the bot to quit the channel
     if data.find('!Bot quit') != -1:
-        irc.send ('PRIVMSG ' + channel + ' :Fine, if you don\'t want me here...\r\n' )
+        irc.send ('PRIVMSG ' + channel + ' :Alright, I\'ll quit then...\r\n' )
         irc.send('QUIT\r\n')
 
     #Says hello back
     if data.find('!hello' or '!hi') != -1:
         irc.send('PRIVMSG ' + channel + ' :Hello\r\n')
+        
+    if data.find('!help' or '!Help') != -1:
+        irc.send(('PRIVMSG ' + channel + ' :All commands begin with ! and are as follows:'  +
+            'hi, hello, slaps, and  8ball or 8b\r\n')
 
 
     ###################################################################################################
@@ -79,23 +87,22 @@ while True:
     ###################################################################################################
 
     if data.find('!slaps') != -1:
-        irc.send('PRIVMSG ' + channel + ' :Look mother fucker, I don\'t have time for your shit...\r\n')
-
-    if data.find('!angry8' or '!a8ball') != -1:
-        angry8ball()
+        irc.send('PRIVMSG ' + channel + ' :Come on man, why you got to be like that?
+        
+    if data.find('!8b' or '!8ball') != -1:
+        8ball()
 
 
     ##################################################################################################
-    ###--------------------------------------METHODS BELOW-----------------------------------------###
+    ###---------------------------------METHODS / FUNCTIONS BELOW----------------------------------###
     ##################################################################################################
 
-    def angry8ball():
-        ball_responses = ["Try again, wanker!", "Meh, kinda", "Fuckin' a right!", "Shit yeah",
-                      "yes", "No", "Bitch, please", "Shit, idk", "Who cares", "How the fuck am I supposed to know?",
-                      "I'm hazy, try again", "Without a doubt, you queer", "Idk, prob", "IDK TRY HARDER NEXT TIME",
-                      "Fuck no", "Outlook good, now fuck off", "Don't fuckin' count on it",
-                      "Cannot predict now, what am I, an fucking geanie?", "Yes definitely, now piss off",
-                      "Better not tell you now, you'll piss yourself", "Outlook not good. Don't be a pussy, roll again."]
+    def 8ball():
+        ball_responses = ["Yes.", "Reply hazy, try again.", "Without a doubt." "My sources say no.", 
+        "As I see it, yes.", "You may rely on it.", "Concentrate and ask again.", "Outlook not so good.",
+        "It is decidedly so.", "Better not tell you now.", "Very doubtful.", "Yes, definitely.", 
+        "It is certain.", "Cannot predict now.", "Most likely.", "Ask again later.", "My reply is no.", 
+        "Outlook good.", "Don\'t count on it."]
 
         #Prints to the IRC chat
         irc.send('PRIVMSG ' + channel + ' ' + random.choice(ball_responses) + '\r\n')
